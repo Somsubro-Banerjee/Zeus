886706c5315f628ce32033d5283441ea8567298e
Som <somsubrob@gmail.com>
2019-03-11 21:47:07 +0530

    File uploaded on 11-03-2019 at 21:47 hrs

--- app/src/main/java/com/project/reloaded/zeus/ProfileActivity.java
+++ app/src/main/java/com/project/reloaded/zeus/ProfileActivity.java
@@ -1,11 +1,15 @@
 package com.project.reloaded.zeus;
 
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.drawable.AnimationDrawable;
 import android.nfc.NfcAdapter;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -14,9 +18,15 @@ import android.nfc.NdefRecord;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
+import android.widget.Toast;
+
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Locale;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -37,11 +47,11 @@ public class ProfileActivity extends AppCompatActivity {
         animationDrawable.start();
 
 
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this);//t well this still is still not implemented......
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);// well this still is still not implemented......
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pay();
+
             }
         });
 
@@ -56,11 +66,124 @@ public class ProfileActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        enableForeGroundDispatchSystem();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        disableForeGroundDIspatchSystem();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if(intent.hasExtra(NfcAdapter.EXTRA_TAG)){
+            Toast.makeText(this, "NFCIntent", Toast.LENGTH_SHORT).show();
+
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            NdefMessage ndefMessage = createNdefMessage("My content");
+            writNdefNessage(tag, ndefMessage);
+
+        }
+    }
+
+    private void enableForeGroundDispatchSystem(){
+        Intent intent = new Intent(this,ProfileActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,0);
+        IntentFilter[] intentFilters = new IntentFilter[] {};
+        nfcAdapter.enableForegroundDispatch(this,pendingIntent,intentFilters,null);
+    }
+
+    private void disableForeGroundDIspatchSystem(){
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    private void formatTag(Tag tag, NdefMessage ndefMessage){
+        try{
+            NdefFormatable ndefFormatable = NdefFormatable.get(tag);
+            if(ndefFormatable == null)
+            {
+                Toast.makeText(this, "Tag is not formatable", Toast.LENGTH_SHORT).show();
+                ndefFormatable.connect();
+                ndefFormatable.format(ndefMessage);
+                ndefFormatable.close();
+            }
+
+        }catch (Exception e) {
+
+            Log.e("FormatTag",e.getMessage());
+
+        }
+
+
+    }
+    private void writNdefNessage(Tag tag, NdefMessage ndefMessage){
+        try{
+            if(tag==null){
+                Toast.makeText(this , "Tag is not writable", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null)
+            {
+                formatTag(tag,ndefMessage);
+            }
+            else
+            {
+                ndef.connect();
+                if(!ndef.isWritable()){
+                    Toast.makeText(this, "Tag is not writable", Toast.LENGTH_SHORT).show();
+                    ndef.close();
+                    return;
+                }
+                ndef.writeNdefMessage(ndefMessage);
+                ndef.close();
+                Toast.makeText(this, "Tag is writable", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }catch(Exception e){
+            Log.e("writeNdefMessage",e.getMessage());
+        }
+    }
+    private NdefRecord createTextRecord(String content){
+        try{
+            byte[] language;
+            language = Locale.getDefault().getLanguage().getBytes("UTF-8");
+            final byte[] text =content.getBytes("UTF-8");
+            final int languageSize = language.length;
+            final int textLength = text.length;
+            final ByteArrayOutputStream payload = new ByteArrayOutputStream(1 + languageSize + textLength);
+
+            payload.write((byte) (languageSize & 0x1F));
+            payload.write(language,0,languageSize);
+            payload.write(text,0,textLength);
+
+            return new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_TEXT, new byte[0],payload.toByteArray());
+
+
+
+        } catch (UnsupportedEncodingException e) {
+            Log.e("creteTextRecord", e.getMessage());
+        }
+        return null;
+    }
+
+    private NdefMessage createNdefMessage(String content){
+        NdefRecord ndefRecord = createTextRecord(content);
+        NdefMessage ndefMessage = new NdefMessage(new NdefRecord[]{ndefRecord});
+        return ndefMessage;
+    }
+
     public void openMainActivity(){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
-    public void pay(){//see  i said still not implemented......this is the only task now  LOL
 
-    }
 }
